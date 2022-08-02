86a9148057b67936fb7f51ad1263212a0ad62ae1
Jasmin Bannister <jasminbannister@Jasmins-MacBook-Pro.local>
2022-08-02 10:53:44 +0100

    GET article_id

--- controller/newspaper.js
+++ controller/newspaper.js
@@ -1,7 +1,16 @@
-const { fetchTopics } = require("../model/newpaper")
+const { fetchTopics, fetchArticleById } = require("../model/newpaper");
 
 exports.getTopics = (req, res) => {
     fetchTopics().then((topicArr) => {
         res.status(200).send({topics : topicArr});        
     })
-}
+    .catch((err) => next(err));
+};
+
+exports.getArticleById = (req, res, next) => {
+    const articleId = req.params['article_id'];
+    fetchArticleById(articleId).then((article)=> {
+       res.status(200).send({article: article})
+    })
+    .catch((err) => next(err));
+};
